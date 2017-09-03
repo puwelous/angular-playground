22d1e8d2e72ddb64f49ba96dbabecbeba1c75115
Pavol Dano <pavol.dano@gmail.com>
2017-09-03 20:57:20 +0200

    apply implemented components, show cards for Angular, Typescript, Weather data (SO and local, too)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {SearchService} from "./core/services/search.service";
+import {WeatherService} from "./core/services/weather.service";
 import {AppRoutingModule} from "./app.routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {LayoutModule} from "./core/layout/layout.module";
 import { SearchComponent } from './search/search.component';
 
+import { SoQuestionsCardComponent } from './dashboard/cards/so-questions-card.component';
+import { MixedCardComponent } from './dashboard/cards/mixed-card.component';
+import { SoListItemComponent } from './dashboard/cards/items/so-list-item.component';
+import { WeatherListItemComponent } from './dashboard/cards/items/weather-list-item.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SearchComponent
+    SearchComponent,
+
+    SoQuestionsCardComponent,
+    MixedCardComponent,
+    SoListItemComponent,
+    WeatherListItemComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +35,7 @@ import { SearchComponent } from './search/search.component';
     LayoutModule,
     AppRoutingModule
   ],
-  providers: [SearchService],
+  providers: [SearchService, WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { QueryDefinition } from '../query-definition';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  queryTermTypescript: string = 'TypeScript';
 
-  ngOnInit() {
+  queryTermAngular: string = 'AngularJS';
+
+  queryTermWeather: string = 'Weather';
+
+  queryDefinitionTypeScript: QueryDefinition;
+
+  queryDefinitionAngular: QueryDefinition;
+
+  queryDefinitionWeather: QueryDefinition;
+
+  constructor() {
+    this.queryDefinitionTypeScript = new QueryDefinition(10, this.queryTermTypescript);
+    this.queryDefinitionAngular = new QueryDefinition(10, this.queryTermAngular);
+    this.queryDefinitionWeather = new QueryDefinition(10, this.queryTermWeather);
   }
 
 }
